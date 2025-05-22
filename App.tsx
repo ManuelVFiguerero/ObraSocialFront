bf8cb06fa19aab4743faff772402529bebd0e0f0
LautaroCasalini <lcasalini@uade.edu.ar>
2025-05-22 11:07:51 -0300

    Agregado de pantalla de Notificaciones por tipo

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import Toast from 'react-native-toast-message';
 import ReserveAppointmentLocationScreen from './src/screens/ReserveAppointmentLocationScreen';
 import ReserveAppointmentScreen from './src/screens/ReserveAppointmentScreen';
 import ContactUsScreen from './src/screens/ContactUsScreen';
+import AllNotificationsScreen from './src/screens/AllNotificationsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -61,6 +62,10 @@ export default function App() {
         <Stack.Screen 
           name="Notifications"
           component={NotificationsScreen}
+        />
+        <Stack.Screen
+         name="AllNotifications"
+         component={AllNotificationsScreen} 
         />
         <Stack.Screen 
           name="AboutUs"
